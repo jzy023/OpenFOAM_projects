6226fb9a55c44546b5805e6c3a732172970e85fd
jzy023 <noreff@DESKTOP-BTFG136>
2023-09-21 12:42:59 +0200

    fix: ADtank snappyHexMesh fixed

--- v2206/Projects/ADTank/runAll.sh
+++ v2206/Projects/ADTank/runAll.sh
@@ -1,3 +1,21 @@
 blockMesh
-decomposePar
-mpirun -np 4 DPMFoam -parallel > log &
+surfaceFeatures
+snappyHexMesh -overwrite
+simpleFoam
+foamToVTK
+# decomposePar
+# mpirun -np 4 DPMFoam -parallel > log &
+
+# #!/bin/sh
+# cd ${0%/*} || exit 1    # Run from this directory
+
+# # Source tutorial run functions
+# . $WM_PROJECT_DIR/bin/tools/RunFunctions
+
+# application=$(getApplication)
+
+# runApplication blockMesh
+# runApplication surfaceFeatures
+# runApplication snappyHexMesh -overwrite
+# runApplication decomposePar
+# runParallel $(getApplication)
