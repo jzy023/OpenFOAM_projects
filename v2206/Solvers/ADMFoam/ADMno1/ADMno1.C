50b3ce1f98327de6756dad944d3d08de23179c46
jzy023 <noreff@DESKTOP-BTFG136>
2024-02-28 15:22:31 +0100

    add: cleanups; add hotFloor case; volume fraction dictionary option

---
+++ v2206/Solvers/ADMFoam/ADMno1/ADMno1.C
@@ -0,0 +1,563 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2013-2017 OpenFOAM Foundation
+    Copyright (C) 2019-2020 OpenCFD Ltd.
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+ 
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+ 
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+ 
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+ 
+\*---------------------------------------------------------------------------*/
+
+#include "ADMno1.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+ 
+namespace Foam
+{
+    defineTypeNameAndDebug(ADMno1, 0);
+    // defineRunTimeSelectionTable(ADMno1, fvMesh);
+}
+ 
+const Foam::word Foam::ADMno1::propertiesName("admno1Properties");
+
+
+// * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::ADMno1::ADMno1
+(
+    const fvMesh& mesh,
+    const IOdictionary& ADMno1Dict
+)
+:
+    IOdictionary(ADMno1Dict),
+    para_(ADMno1Dict.get<word>("mode")),
+    Sc_(ADMno1Dict.lookupOrDefault("Sc", 0.2)),
+    R_(ADMno1Dict.lookupOrDefault("R", 0.083145)),
+    KP_(ADMno1Dict.lookupOrDefault("Kpip", 5e4)),
+    Vfrac_(ADMno1Dict.lookupOrDefault("Vfrac", 0.088235)),
+    Pext_
+    (
+        "Pext", 
+        dimPressure,
+        ADMno1Dict.lookupOrDefault("Pext", 1.013)
+    ),
+    fac_
+    (
+        IOobject
+        (
+            "fac",
+            mesh.time().timeName(),
+            mesh,
+            IOobject::READ_IF_PRESENT,
+            IOobject::NO_WRITE
+        ),
+        mesh,
+        dimensionedScalar
+        (
+            "facDefault", 
+            dimless, 
+            ADMno1Dict.lookupOrDefault("fac", 1)
+        )
+    ),
+    pH_
+    (
+        IOobject
+        (
+            "pH",
+            mesh.time().timeName(),
+            mesh,
+            IOobject::READ_IF_PRESENT,
+            IOobject::AUTO_WRITE
+        ),
+        mesh,
+        dimensionedScalar
+        (
+            "pHDefault", 
+            dimless, 
+            ADMno1Dict.lookupOrDefault("pH", 7.26)
+        )
+    ),
+    Scat_(ADMno1Dict.lookupOrDefault("Scat", 0.00)),
+    San_(ADMno1Dict.lookupOrDefault("San", 0.0052))
+{
+
+    Info<< "\nSelecting ADM no1 operation mode " << ADMno1Dict.get<word>("mode") << endl;
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    //- Main substances concentration initialization
+
+    Info<< "Reading ADM no1 initial concentrations for soluables" << endl;
+
+    label iNames = 0;
+    label nSpecies = namesSoluable.size() + namesParticulate.size();
+
+    YPtrs_.resize(nSpecies);
+
+    forAll(namesSoluable, i)
+    {
+        YPtrs_.set
+        (
+            i,
+            new volScalarField
+            (
+                IOobject
+                (
+                    namesSoluable[i], // IOobject::groupName(namesSoluable[i]),
+                    mesh.time().timeName(),
+                    mesh,
+                    IOobject::MUST_READ,
+                    IOobject::AUTO_WRITE
+                ),
+                mesh
+            )
+        );
+    }
+
+    iNames += namesSoluable.size();
+
+    //-  Read particulates
+
+    Info<< "Reading ADMno1 initial concentrations for particulates" << endl;
+
+    forAll(namesParticulate, i)
+    {
+        YPtrs_.set
+        (
+            i + iNames,
+            new volScalarField
+            (
+                IOobject
+                (
+                    namesParticulate[i],
+                    mesh.time().timeName(),
+                    mesh,
+                    IOobject::MUST_READ,
+                    IOobject::AUTO_WRITE
+                ),
+                mesh
+            )
+        );
+    }
+
+    //- Initializing derivatives
+
+    dYPtrs_.resize(nSpecies);
+
+    for(label i = 0; i < nSpecies; i++)
+    {
+        dYPtrs_.set
+        (
+            i,
+            new volScalarField::Internal
+            (
+                IOobject
+                (
+                    "d" + YPtrs_[i].name(),
+                    mesh.time().timeName(),
+                    mesh,
+                    IOobject::NO_READ,
+                    IOobject::NO_WRITE
+                ),
+                mesh,
+                dimensionedScalar
+                (
+                    YPtrs_[0].dimensions()/dimTime, 
+                    Zero
+                )
+            )
+        );
+    }
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    //-  Gaseuoses initialization
+
+    Info<< "Reading ADM no1 initial concentrations for gaseuoses" << endl;
+
+    GPtrs_.resize(namesGaseous.size());
+
+    forAll(namesGaseous, i)
+    {
+        GPtrs_.set
+        (
+            i,
+            new volScalarField
+            (
+                IOobject
+                (
+                    namesGaseous[i],
+                    mesh.time().timeName(),
+                    mesh,
+                    IOobject::READ_IF_PRESENT,
+                    IOobject::AUTO_WRITE
+                ),
+                mesh,
+                dimensionedScalar
+                (
+                    namesGaseous[i] + "Default", 
+                    YPtrs_[0].dimensions(),
+                    para_.Gini(i)
+                )
+            )
+        );
+    }
+    
+    //- Initializing derivatives
+
+    dGPtrs_.resize(namesGaseous.size());
+
+    for(label i = 0; i < namesGaseous.size(); i++)
+    {
+        dGPtrs_.set
+        (
+            i,
+            new volScalarField::Internal
+            (
+                IOobject
+                (
+                    "d" + GPtrs_[i].name(),
+                    mesh.time().timeName(),
+                    mesh,
+                    IOobject::NO_READ,
+                    // IOobject::NO_WRITE
+                    IOobject::AUTO_WRITE
+                ),
+                mesh,
+                dimensionedScalar
+                (
+                    GPtrs_[0].dimensions()/dimTime, 
+                    Zero
+                )
+            )
+        );
+    }
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    //-  Electrolytes (calculated) initialization
+
+    Info<< "Initializing concentrations for electrolyte" << endl;
+
+    EPtrs_.resize(namesElectrolyte.size());
+
+    forAll(namesElectrolyte, i)
+    {
+        EPtrs_.set
+        (
+            i,
+            new volScalarField
+            (
+                IOobject
+                (
+                    namesElectrolyte[i],
+                    mesh.time().timeName(),
+                    mesh,
+                    IOobject::READ_IF_PRESENT,
+                    IOobject::NO_WRITE
+                ),
+                mesh,
+                dimensionedScalar
+                (
+                    namesElectrolyte[i] + "Default", 
+                    YPtrs_[0].dimensions(),
+                    para_.Eini(i)
+                )
+            )
+        );
+    }
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    //-  Medians initialization
+
+    Info<< "Initializing concentrations for medians" << endl;
+
+    MPtrs_.resize(namesMedians.size());
+
+    forAll(namesMedians, i)
+    {
+        MPtrs_.set
+        (
+            i,
+            new volScalarField
+            (
+                IOobject
+                (
+                    namesMedians[i],
+                    mesh.time().timeName(),
+                    mesh,
+                    IOobject::READ_IF_PRESENT,
+                    IOobject::AUTO_WRITE
+                ),
+                mesh,
+                dimensionedScalar
+                (
+                    namesMedians[i] + "Default", 
+                    YPtrs_[0].dimensions(),
+                    para_.Mini(i)
+                )
+            )
+        );
+    }
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    //- Set up temperal concentration fields
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    //- Inhibition coeffs initialization
+
+    IPtrs_.resize(8);
+
+    for (int i = 0; i < 8; i++)
+    {
+        IPtrs_.set
+        (
+            i,
+            new volScalarField
+            (
+                IOobject
+                (
+                    "Inh" + Foam::name(i),
+                    mesh.time().timeName(),
+                    mesh,
+                    IOobject::NO_READ,
+                    IOobject::NO_WRITE// TODO: choose if writing
+                ),
+                mesh,
+                dimensionedScalar
+                (
+                    dimless, 
+                    Zero
+                )
+            )
+        );
+    }
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    //- Kinetic rate initialization
+    
+    KRPtrs_.resize(19);
+
+    for (int i = 0; i < 19; i++)
+    {
+        KRPtrs_.set
+        (
+            i,
+            new volScalarField::Internal
+            (
+                IOobject
+                (
+                    "RRs" + Foam::name(i),
+                    mesh.time().timeName(),
+                    mesh,
+                    IOobject::NO_READ,
+                    IOobject::NO_WRITE
+                ),
+                mesh,
+                dimensionedScalar
+                (
+                    dYPtrs_[0].dimensions(), 
+                    Zero
+                )
+            )
+        );
+    }
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    //- Gas trasfer rate initialization
+    
+    GRPtrs_.resize(3);
+
+    for (int i = 0; i < 3; i++)
+    {
+        GRPtrs_.set
+        (
+            i,
+            new volScalarField::Internal
+            (
+                IOobject
+                (
+                    "GRs" + Foam::name(i),
+                    mesh.time().timeName(),
+                    mesh,
+                    IOobject::NO_READ,
+                    IOobject::NO_WRITE
+                ),
+                mesh,
+                dimensionedScalar
+                (
+                    dGPtrs_[0].dimensions(), 
+                    Zero
+                )
+            )
+        );
+    }
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    para_.setParaDim(YPtrs_[0].dimensions());
+
+    nIaa_ = 3.0 / (para_.pHL().ULaa - para_.pHL().LLaa);  // aa
+    nIac_ = 3.0 / (para_.pHL().ULac - para_.pHL().LLac);  // ac
+    nIh2_ = 3.0 / (para_.pHL().ULh2 - para_.pHL().LLh2);  // h2
+
+    // >>> TEST
+    // dYPtrs_[7].field() = 0.01*2.5055e-7;
+
+}
+
+
+// * * * * * * * * * * * * * * * * Selectors * * * * * * * * * * * * * * * * //
+ 
+Foam::autoPtr<Foam::ADMno1> Foam::ADMno1::New
+(
+    const fvMesh& mesh
+)
+{
+    IOdictionary ADMno1Dict
+    (
+        IOobject
+        (
+            propertiesName,
+            mesh.time().constant(),
+            mesh,
+            IOobject::READ_IF_PRESENT,
+            IOobject::NO_WRITE,
+            false
+        )
+    );
+
+    // TODO: do it properly!!! with virtual destructors and constructor hash tables 
+    // new keywaord is not gonna last!
+    ADMno1* reactionPtr = new ADMno1(mesh, ADMno1Dict);
+    return autoPtr<ADMno1>(reactionPtr);
+}
+
+
+// * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
+
+void Foam::ADMno1::updateMedians()
+{
+    // Sco2 = SIC - Shco3N
+    MPtrs_[0] = YPtrs_[9] - EPtrs_[4];
+
+    // Snh3 calculated with acid-base
+
+    // Shn4 = SIN - Snh3
+    
+}
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void Foam::ADMno1::clear()
+{
+    forAll(dYPtrs_, i)
+    {
+        dYPtrs_[i] *= 0.0;
+    }
+
+    forAll(dGPtrs_, i)
+    {
+        dGPtrs_[i] *= 0.0;
+    }
+}
+
+void Foam::ADMno1::correct(volScalarField& T)
+{
+    //- update Medians
+    updateMedians();
+
+    //- calculate gas phase transfer rates
+    GasPhaseRate(T);
+
+    //- calculate gas exit rates
+    GasSourceRate(T);
+
+    //- calculate raction rates
+    KineticRate();
+
+    //- calculate dY with STOI
+    SulSourceRate();
+
+}
+
+
+tmp<fvScalarMatrix> Foam::ADMno1::R
+(
+    label i
+) const
+{
+    DimensionedField<scalar, volMesh> dY = dYPtrs_[i];
+
+        tmp<fvScalarMatrix> tSu
+        (
+            new fvScalarMatrix
+            (
+                YPtrs_[i],
+                dY.dimensions()*dimVolume
+                // dimMass/dimTime // <- for compressible flow and uses fvm::ddt(rho, Yi)
+            )
+        );
+
+    fvScalarMatrix& Su = tSu.ref();
+    
+    // https://www.openfoam.com/documentation/guides/latest/api/fvMatrix_8C_source.html#l01708
+    Su += dY; 
+
+    return tSu;
+}; 
+
+
+tmp<fvScalarMatrix> Foam::ADMno1::RG
+(
+    label i
+) const
+{
+    DimensionedField<scalar, volMesh> dG = dGPtrs_[i];
+
+        tmp<fvScalarMatrix> tSu
+        (
+            new fvScalarMatrix
+            (
+                GPtrs_[i],
+                dG.dimensions()*dimVolume
+                // dimMass/dimTime // <- for compressible flow and uses fvm::ddt(rho, Yi)
+            )
+        );
+
+    fvScalarMatrix& Su = tSu.ref();
+    
+    // https://www.openfoam.com/documentation/guides/latest/api/fvMatrix_8C_source.html#l01708
+    Su += dG; 
+
+    return tSu;
+}; 
+
+// ************************************************************************* //
---
+++ v2206/Solvers/ADMFoam/ADMno1/ADMno1Gas.C
@@ -0,0 +1,103 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2013-2017 OpenFOAM Foundation
+    Copyright (C) 2019-2020 OpenCFD Ltd.
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+ 
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+ 
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+ 
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+ 
+\*---------------------------------------------------------------------------*/
+
+#include "ADMno1.H"
+
+// * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
+// TODO: check volume!! 
+
+void Foam::ADMno1::GasPhaseRate(volScalarField& T)
+{
+    //- Thermal condition factor
+
+    volScalarField TopDummy(T);
+
+    TopDummy.dimensions().reset(dimless);
+
+    fac_ = (1.0 / para_.Tbase().value() - 1.0 / TopDummy) / (100.0 * R_);
+
+    GRPtrs_[0] = para_.DTOS() * para_.kLa() 
+               * (YPtrs_[7] - R_ * TopDummy * GPtrs_[0] * para_.KH().h2 * exp(-4180.0 * fac_));
+
+    GRPtrs_[1] = para_.DTOS() * para_.kLa() 
+               * (YPtrs_[8] - R_ * TopDummy * GPtrs_[1] * para_.KH().ch4 * exp(-14240.0 * fac_));
+
+    GRPtrs_[2] = para_.DTOS() * para_.kLa() // Sco2 instead of SIC
+               * (MPtrs_[0] - R_ * TopDummy * GPtrs_[2] * para_.KH().co2 * exp(-19410.0 * fac_));
+            // * (YPtrs_[9] - R_ * TopDummy * GPtrs_[2] * para_.KH().co2 * exp(-19410.0 * fac_));
+
+}
+
+
+void Foam::ADMno1::GasSourceRate(volScalarField& T)
+{
+
+    // field of cell volume for mesh 
+    scalarField volMeshField = GPtrs_[0].mesh().V().field();            
+
+    // particle scaled gas volume
+    scalarField volGas = volMeshField / (1 + (1/Vfrac_)); 
+
+    // particle scaled liquid volume
+    scalarField volLiq = volMeshField / (1 + Vfrac_);
+
+    // particle scaled pipe resistance
+    volScalarField kp = 0.0*fac_;
+
+    kp.dimensions().reset(dimVolume/dimTime/dimPressure);
+
+    kp.field() = KP_ * (volMeshField / (volGas + volLiq)); // <---- this would be calculated
+
+    //- gas pressure
+    volScalarField TopDummy(T);
+
+    TopDummy.dimensions().reset(dimless); 
+
+    volScalarField Ph2o = GPtrs_[0];
+
+    Ph2o.field() = para_.KH().h2 * exp(5290.0 * fac_ * 100 * R_);
+
+    volScalarField Pgas = (GPtrs_[0] / 16.0 + GPtrs_[1] / 64.0 + GPtrs_[2]) * R_ * TopDummy + Ph2o;
+
+    Pgas.dimensions().reset(dimPressure);
+
+    volScalarField qGasLocal = kp * (Pgas - Pext_);
+    //  volScalarField qGasLocal = kp * (Pgas - Pext_)*(Pgas/Pext_); 
+
+	qGasLocal.min(0.0);
+
+    dGPtrs_[0].field() = GRPtrs_[0].field() * volLiq / volGas - GPtrs_[0].field() * qGasLocal.field() / volGas;
+
+    dGPtrs_[1].field() = GRPtrs_[1].field() * volLiq / volGas - GPtrs_[1].field() * qGasLocal.field() / volGas;
+
+    dGPtrs_[2].field() = GRPtrs_[2].field() * volLiq / volGas - GPtrs_[2].field() * qGasLocal.field() / volGas;
+    
+};
+
+
+// ************************************************************************* //
